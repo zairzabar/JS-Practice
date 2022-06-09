85da1e0a9b0cea382448fb2b8a1198f0efe8eecc
zairzabar <zuber.ansari.997@gmail.com>
2022-06-10 01:49:41 +0530

    Tried more Katas

--- src/script.js
+++ src/script.js
@@ -327,3 +327,9 @@ function find_average(array) {
   }
   return sum/array.length
 }
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
