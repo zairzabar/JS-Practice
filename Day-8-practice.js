5b3e1851666a00001f95ada1edcec3b6d3785d89
zairzabar <zuber.ansari.997@gmail.com>
2022-06-16 16:41:02 +0530

    Insertion sort

--- Day-8-practice.js
+++ Day-8-practice.js
@@ -24,4 +24,19 @@ function bubbleSort(arr) {
     return arr
 }
 
-console.log(bubbleSort([1, 5, 6, 4, 3, 8, 7, 3, 2, 3, 5]));
+// console.log(bubbleSort([1, 5, 6, 4, 3, 8, 7, 3, 2, 3, 5]));
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        for (let j = i; j > 0; j--) {
+            if (array[j] < array[j-1]) {
+                [array[j], array[j-1]] = [array[j-1], array[j]]
+            } else {
+                break
+            }
+        }
+    }
+    return array
+}
+
+console.log(insertionSort([1, 5, 6, 4, 3, 8, 7, 3, 2, 3, 5]));
