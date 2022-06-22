7182b8cbfcf7e92a7ecabe1d87fc585b7b90dc01
zairzabar <zuber.ansari.997@gmail.com>
2022-06-23 05:05:21 +0530

    solved another Kata

--- Day-9-practice.js
+++ Day-9-practice.js
@@ -1,9 +1,11 @@
+// Convert the number to reversed array of digits
 function digit(n) {
     return n.toString().split('').reverse().map(Number)
 }
 
 // console.log(digit(45699));
 
+// Find the next perfect square
 function findNextSquare(sq) {
     let sqrt = Math.sqrt(sq)
     let nextSquare = sqrt+1
@@ -14,4 +16,18 @@ function findNextSquare(sq) {
     }
 }
 
-console.log(findNextSquare(24));
+// console.log(findNextSquare(24));
+
+// A Needle in the Haystack
+
+function findNeedle(haystack) {
+    let msg
+    for (let i = 0; i < haystack.length; i++) {
+        if (haystack[i] === 'needle') {
+            msg = console.log(`found the needle at position ${i}`);
+        }
+    }
+    return msg
+}
+
+findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"])
