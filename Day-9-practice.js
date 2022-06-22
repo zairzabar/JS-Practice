9473669d261cc290c620103d49661df8ec9a5513
zairzabar <zuber.ansari.997@gmail.com>
2022-06-22 05:46:09 +0530

    solved second Kata

--- Day-9-practice.js
+++ Day-9-practice.js
@@ -2,4 +2,16 @@ function digit(n) {
     return n.toString().split('').reverse().map(Number)
 }
 
-console.log(digit(45699));
+// console.log(digit(45699));
+
+function findNextSquare(sq) {
+    let sqrt = Math.sqrt(sq)
+    let nextSquare = sqrt+1
+    if (Number.isInteger(Math.sqrt(sq))) {
+      return nextSquare * nextSquare
+    } else {
+        return -1
+    }
+}
+
+console.log(findNextSquare(24));
