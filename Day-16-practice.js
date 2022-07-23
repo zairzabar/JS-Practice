70d2015007bf5fd3395622b030699b8724ac6019
zairzabar <zuber.ansari.997@gmail.com>
2022-07-23 20:47:24 +0530

    Previous practice

---
+++ Day-16-practice.js
@@ -0,0 +1,35 @@
+function findUnique(arr) {
+    let hm = {}
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!hm[arr[i]]) {
+            hm[arr[i]] = 1
+        } else {
+            hm[arr[i]] += 1
+        }
+    }
+
+    let count = 0
+    
+    for (let j = 0; j < arr.length; j++) {
+        if (hm[arr[j]] > 1) {
+            return true
+        }
+        return false
+    }
+}
+
+// console.log(findUnique([3,2,1,2,3]));
+
+function findSum(arr, n) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[i] + arr[j] == n) {
+                return [arr[i], arr[j]]
+            }
+        }
+    }
+    return -1
+}
+
+console.log(findSum([1, 2, 3, 4, 5, 6], 11));
