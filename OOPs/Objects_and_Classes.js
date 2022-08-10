dbc9959670b9bb46582e25ecae15bbd1bc09ffbc
zairzabar <zuber.ansari.997@gmail.com>
2022-08-10 18:24:51 +0530

    Learning OOPs concepts. Starting off with this keyword.

---
+++ OOPs/Objects_and_Classes.js
@@ -0,0 +1,42 @@
+class Product {
+    constructor(n, p) {
+        console.log("Constructor is being called directly from the class name");
+        // constructor will be the first function to be called for creating an object
+        this.name = n;
+        this.price = p;
+    }
+
+    displayProduct() {
+        console.log(`The name of the product is ${this.name}`)
+        console.log(`The price of the product is ${this.price}`)
+    }
+
+    buyProduct() {
+
+    }
+}
+
+// let iphone = new Product('Iphone', 100000)
+// iphone.displayProduct()
+
+class ComplexNumbers {
+    constructor(rnum, inum) {
+        this.real = rnum;
+        this.imag = inum
+    }
+
+    add(c) {
+        this.real += c.real;
+        this.imag += c.imag
+    }
+
+    display() {
+        console.log(`${this.real} + i${this.imag}`);
+    }
+}
+
+let c1 = new ComplexNumbers(5, 3)
+let c2 = new ComplexNumbers(4, 5)
+
+c1.add(c2)
+c1.display()
