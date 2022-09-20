53f43a81947abda9a7d7021c435192b95767b0dc
Ouzair Ansari <88282547+zairzabar@users.noreply.github.com>
2022-09-21 02:46:54 +0530

    practice

--- practice.js
+++ practice.js
@@ -9,3 +9,24 @@ var buildArray = function(nums) {
 };
 
 buildArray([0,2,1,5,3,4])
+
+function binarySearch(arr, x) {
+	let start = 0 // starting position
+  let end = arr.length - 1 // end position
+  
+  
+  while (start <= end) {
+  	let mid = (Math.floor((start + end) / 2)) // mid position
+    
+    if (arr[mid] == x) return mid // returning the index of the mid position if it matches with the given value
+    
+    if (x < arr[mid]) { // checking if the value is smaller than mid
+    	end = mid - 1 // eliminating the right side
+    } else { // checking if the value is greater than mid
+    	start = mid + 1 // then eliminating the left side
+    }
+  }
+  return -1 // returning -1 if nothing matches
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], ))
